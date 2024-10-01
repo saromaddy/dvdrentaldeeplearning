47dcedbbf7130d4d31181daf2aa99247320b499a
Saravana Kumar T <saromaddymca@gmail.com>
2024-10-01 16:46:29 +0530

    new commit sql

--- Data_query script.sql
+++ Data_query script.sql
@@ -1,18 +1,3 @@
-inner join rental r on cu.customer_id = r.customer_id
-inner join payment p on cu.customer_id = p.customer_id
-inner join inventory i on i.inventory_id = r.inventory_id
-inner join film on i.film_id = film.film_id
-inner join film_category fact on film.film_id = fact.film_id
-inner join category cat on fact.category_id = cat.category_id
-inner join language as l on l.language_id = film.language_id
-
-
-
-select cu.customer_id, cu.active, r.customer_id, r.rental_date, r.return_date from customer cu
-join rental r on r.customer_id = cu.customer_id
-
-
-
 create table film_category_language as
 select f.film_id, f.title, f.rental_duration, f.rental_rate, f.length, f.replacement_cost, f.rating, cat.name as category, l.name as language from film f
 join film_category fact on fact.film_id = f.film_id
