5f4989cf483cbe713b27c2a4b635697ee2902b89
saromaddy <saromaddymca@gmail.com>
2024-10-01 15:31:38 +0530

    sdf

--- Data_query script.sql
+++ Data_query script.sql
@@ -1,4 +1,3 @@
-select cu.customer_id,cu.active,cu.create_date,cu.last_update,r.rental_date, r.return_date, p.amount, cat.name as category, l.name as language from customer cu
 inner join rental r on cu.customer_id = r.customer_id
 inner join payment p on cu.customer_id = p.customer_id
 inner join inventory i on i.inventory_id = r.inventory_id
